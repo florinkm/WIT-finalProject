f5c237ab437c7f79dff3dfdb67e618f261067493
florinkm <florinmanalu@gmail.com>
2023-06-06 23:40:06 +0700

    admin-kategori

--- ecomm/toko/migrations/0001_initial.py
+++ ecomm/toko/migrations/0001_initial.py
@@ -22,7 +22,7 @@ class Migration(migrations.Migration):
                 ('deskripsi', models.TextField()),
                 ('gambar', models.ImageField(upload_to='product_pics')),
                 ('label', models.CharField(choices=[('NEW', 'primary'), ('SALE', 'secondary'), ('BEST', 'danger')], max_length=4)),
-                ('kategori', models.CharField(choices=[('S', 'Shirt'), ('SW', 'Sport wear'), ('OW', 'Outwear')], max_length=2)),
+                ('kategori', models.CharField(choices=[('DL', 'Detergent Laundry'), ('AN', 'Anti Noda'), ('PL', 'Pelengkap Laundry'), ('ML', 'Mesin Laundry')], max_length=2)),
             ],
         ),
     ]
